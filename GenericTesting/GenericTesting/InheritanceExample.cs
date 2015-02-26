292bdb1d071129db59a9d2e44dc84ccfcbc58a2a
djangojazz <brettmorin2@gmail.com>
2015-02-25 19:55:25 -0800

    Example of Base Inheritance Setting up a base object for use in the child class to echo back to the base class

---
+++ GenericTesting/GenericTesting/InheritanceExample.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericTesting
+{
+    
+    public class Sub : Base
+    {
+        public Sub() : base()
+        {
+            // If you are inheriting and the base object is accessible you may change it in the child class
+            Resource = "Changed";  
+            Output += "SUB Resource is: " + Resource + Environment.NewLine;
+        }
+
+        // Or I may overload a constructor to then overload a base class and call that.
+        // I think this may be what you want as you would create a NEW constructor in the base and this would not affect existing code
+        // plus this way you could pass in and inject the logic from the sub class to call the base constructor directly and then have 
+        // it all pass through.  Notice the timing will always call the base first to.
+        public Sub(string resource) : base(resource) 
+        {
+            Output += "SUB Resource is: " + Resource + Environment.NewLine;
+        }
+    }
+
+    public class Base
+    {
+        public string Resource;
+        public string Output;
+
+        public Base()
+        {
+            Output += "BASE Resource is: " + Resource + Environment.NewLine;
+        }
+
+        public Base(string resource)
+        {
+            Resource = resource;
+            Output += "BASE Overloaded Constructor Resource is: " + Resource + Environment.NewLine;
+        }
+    }
+}
--- GenericTesting/GenericTesting/Program.cs
+++ GenericTesting/GenericTesting/Program.cs
@@ -10,6 +10,27 @@ namespace GenericTesting
     {
         static void Main(string[] args)
         {
+            Sub x = new Sub();
+            Sub y = new Sub("second way");
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("-------------");
+            sb.AppendLine("First Way");
+            sb.AppendLine("-------------");
+            sb.AppendLine();
+
+            sb.AppendLine(x.Output);
+
+            sb.AppendLine("-------------");
+            sb.AppendLine("Second Way");
+            sb.AppendLine("-------------");
+            sb.AppendLine();
+
+            sb.AppendLine(y.Output);
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
         }
     }
 }
