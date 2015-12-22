b58dbbb7252f9f2895d1ff63f7bd0b49757998ad
djangojazz <brettmorin2@gmail.com>
2015-12-22 10:38:17 -0800

    Added generic POCO class

--- GenericTesting/GenericTesting/Program.cs
+++ GenericTesting/GenericTesting/Program.cs
@@ -10,13 +10,32 @@ namespace GenericTesting
 {
     class Program
     {
+        static List<POCO> GetPOCOs()
+        {
+            return new List<POCO>
+            {
+                new POCO { Id = 1, Name = "John", Description = "basic" },
+                new POCO { Id = 2, Name = "Jane", Description = "more" },
+                new POCO { Id = 3, Name = "Joey", Description = "advanced" }
+            };
+        }
+        
         static void Main(string[] args)
         {
-            var appointmentDate = "2013-06-13T00:00:00";
-            var pstTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Pacific Standard Time");
+            var pocos = GetPOCOs();
+            var ints = new[] { 1, 2, 3 };
 
-            var pstDate = pstTimeZone.ConvertDateFromTimeZoneToUTCElseDefaultUTCNow(appointmentDate);
-            Console.WriteLine(pstDate);
+            var exists = pocos.All(x => ints.Contains(x.Id));
+            Console.WriteLine(exists);
+
+            //var inputString = "Person,Item" + Environment.NewLine + "John,A" + Environment.NewLine + "Jane,B";
+            //var items = inputString.ReturnAMultiLineDictionaryFromAString();
+
+            //var appointmentDate = "2013-06-13";
+            //var pstTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Pacific Standard Time");
+
+            //var pstDate = pstTimeZone.ConvertDateFromTimeZoneToUTCElseDefaultUTCNow(appointmentDate);
+            //Console.WriteLine(pstDate);
 
             //var dicsl = StringBoxingAndUnboxingToADictionary.DictionaryBoxing;
             //var str = dicsl.ReturnAStringFromADictionary();
