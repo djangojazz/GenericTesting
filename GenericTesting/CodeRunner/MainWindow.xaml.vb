902ca42d2d73c14424556e3ab2fce6c676ca6cf0
djangojazz <brettmorin2@gmail.com>
2015-12-22 12:54:11 -0800

    Added CodeRunner project

---
+++ GenericTesting/CodeRunner/MainWindow.xaml.vb
@@ -0,0 +1,18 @@
+﻿Class MainWindow 
+
+    Sub RunCode(sender As Object, e As RoutedEventArgs)
+        'Add code here
+        'Output("Hellow World")
+        Dim input As String = txtInput.Text
+        Output(input)
+    End Sub
+
+    Sub Output(Value As String)
+        txtOutput.Text += Value + vbCrLf
+    End Sub
+
+    Sub ClearOutput(sender As Object, e As RoutedEventArgs)
+        txtOutput.Text = ""
+    End Sub
+
+End Class
