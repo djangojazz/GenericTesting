b6efae6049bc94d64a3abf07a59c12ebd103128f
djangojazz <brettmorin2@gmail.com>
2015-12-23 13:54:37 -0800

    some changes

--- GenericTesting/CodeRunner/MainWindow.xaml.vb
+++ GenericTesting/CodeRunner/MainWindow.xaml.vb
@@ -2,9 +2,21 @@
 
     Sub RunCode(sender As Object, e As RoutedEventArgs)
         'Add code here
-        'Output("Hellow World")
-        Dim input As String = txtInput.Text
-        Output(input)
+        Dim unknownType
+
+        Dim myString As String = "Hello World"
+        Dim myNumber As Integer = 42
+        Dim myDate As Date = DateValue("November 28, 2015")
+
+        Dim myString2 = "Hello World"
+        Dim myNumber2 = 42.5
+        Dim myDate2 = DateValue("November 28, 2015")
+
+        unknownType = myString
+
+        Output(myString)
+        Output(myNumber)
+        Output(myDate)
     End Sub
 
     Sub Output(Value As String)
