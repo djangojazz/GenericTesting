35e30f988998f933db45d47647f55f15d691c211
djangojazz <brettmorin2@gmail.com>
2016-01-03 06:04:09 -0800

    More work in practice area

--- GenericTesting/CodeRunner/MainWindow.xaml.vb
+++ GenericTesting/CodeRunner/MainWindow.xaml.vb
@@ -1,22 +1,12 @@
-﻿Class MainWindow 
+﻿Class MainWindow
+
+    Dim names As ArrayList = New ArrayList()
+
+
 
     Sub RunCode(sender As Object, e As RoutedEventArgs)
         'Add code here
-        Dim unknownType
 
-        Dim myString As String = "Hello World"
-        Dim myNumber As Integer = 42
-        Dim myDate As Date = DateValue("November 28, 2015")
-
-        Dim myString2 = "Hello World"
-        Dim myNumber2 = 42.5
-        Dim myDate2 = DateValue("November 28, 2015")
-
-        unknownType = myString
-
-        Output(myString)
-        Output(myNumber)
-        Output(myDate)
     End Sub
 
     Sub Output(Value As String)
