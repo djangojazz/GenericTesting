45e175f1837be0435564a18357eb6b6a76894a5e
Brett Morin <brettmorin2@gmail.com>
2016-08-16 13:31:54 -0700

    Added a large MVC project

---
+++ GenericTesting/AsyncAspNet/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AsyncAspNet.Controllers
+{
+  public class HomeController : Controller
+  {
+    public async Task<ActionResult> Index()
+    {
+      using (var client = new HttpClient())
+      {
+        var httpMessage = await client.GetAsync("http://www.filipekberg.se/rss/").ConfigureAwait(false);
+
+        var content = await httpMessage.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+        return Content(content);
+      }
+      
+    }
+
+    public ActionResult About()
+    {
+      ViewBag.Message = "Your application description page.";
+
+      return View();
+    }
+
+    public ActionResult Contact()
+    {
+      ViewBag.Message = "Your contact page.";
+
+      return View();
+    }
+  }
+}
---
+++ GenericTesting/AsyncAspNet/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AsyncAspNet.Startup))]
+namespace AsyncAspNet
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
