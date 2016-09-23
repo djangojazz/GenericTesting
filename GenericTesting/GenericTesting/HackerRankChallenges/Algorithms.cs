68c882f8413f4e812a8e1a097968bc0b8c2dcf39
Brett Morin <brettmorin2@gmail.com>
2016-09-23 09:52:53 -0700

    Made child relationship

---
+++ GenericTesting/GenericTesting/HackerRankChallenges/Algorithms.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericTesting.HackerRankChallenges
+{
+  public static class Algorithms
+  {
+    public static void TheBeastChallenge()
+    {
+      Func<decimal, bool> IsValid = (inputNumber) => Decimal.Remainder((inputNumber / 3), 1) == 0 && Decimal.Remainder((inputNumber / 5), 1) == 0 ? true : false;
+      Func<IEnumerable<IEnumerable<string>>, IEnumerable<IEnumerable<string>>> CartesianProduct = (sequences) =>
+      {
+        IEnumerable<IEnumerable<string>> emptyProduct = new[] { Enumerable.Empty<string>() };
+        return sequences.Aggregate(emptyProduct, (accumulator, sequence) => from accseq in accumulator from item in sequence select accseq.Concat(new[] { item }));
+      };
+
+      for (int i = 1; i <= 20; i++)
+      {
+        var combs = CartesianProduct(Enumerable.Repeat(new[] { "5", "3" }, i)).ToList();
+        decimal x = -1;
+        foreach (var item in combs) { Decimal.TryParse(String.Join("", item), out x); if (IsValid(x)) break; else x = -1; }
+
+        Console.WriteLine($"{i} {x}");
+      }
+    }
+  }
+}
--- GenericTesting/GenericTesting/Program.cs
+++ GenericTesting/GenericTesting/Program.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Collections.ObjectModel;
 using System.Web.Script.Serialization;
 using System.Text;
+using static GenericTesting.HackerRankChallenges.Algorithms;
 
 namespace GenericTesting
 {
@@ -28,8 +29,7 @@ namespace GenericTesting
       NewTimer.Elapsed += (sender, e) => Action.Invoke(sender, e);
       TimerList.Add(NewTimer);
     }
-
-
+    
     //DISPOSE
     public void Dispose()
     {
@@ -51,9 +51,7 @@ namespace GenericTesting
       timer.Elapsed += async (sender, e) => await timerDuration();
       timer.Enabled = true;
     }
-    
-    
-    
+
     public static List<T> GetSpecificFields<T>(DataTable dt, string columnName)
     {
       return dt.AsEnumerable().Select(x => (T)(object)x.Field<T>(columnName)).ToList();
@@ -90,6 +88,13 @@ namespace GenericTesting
     {
       public int Id { get; set; }
       public string Desc { get; set; }
+      public List<Order> Orders { get; set; }
+    }
+
+    public class Order
+    {
+      public int Id { get; set; }
+      public string Desc { get; set; }
     }
 
     static List<POC> GetPOCOs()
@@ -98,108 +103,46 @@ namespace GenericTesting
       {
           new POC { Id = 1, Desc = "John"},
           new POC { Id = 2, Desc = "Jane" },
-          new POC { Id = 3, Desc = "\"Joey\"" }
+          new POC { Id = 3, Desc = "Joey" }
+      };
+    }
+
+    static List<Order> GetOrders(int numberOfOrders)
+    {
+      var orders = new List<Order>();
+
+      for (int i = 1; i <= numberOfOrders; i++)
+      {
+        orders.Add(new Order { Id = i, Desc = $"{i} Order" });
+      }
+
+      return orders;
+    }
+
+    static List<POC> GetPOCOsAndOrders()
+    {
+      return new List<POC>
+      {
+          new POC { Id = 1, Desc = "John", Orders = GetOrders(1)},
+          new POC { Id = 2, Desc = "Jane", Orders = GetOrders(2) },
+          new POC { Id = 3, Desc = "Joey" , Orders = GetOrders(3)}
       };
     }
     
-    public class OtherPerson
-    {
-      public int PersonId { get; set; }
-      public string PersonLongName { get; set; }
-      public teOrder Order { get; set; }
-    }
 
     static void Main(string[] args)
     {
-      Func<decimal, bool> IsValid = (inputNumber) =>
-      {
-        return Decimal.Remainder((inputNumber / 3), 1) == 0 && Decimal.Remainder((inputNumber / 5), 1) == 0 ? true : false;
-      };
+      var orders = new List<int> { 2, 3 };
+      var items = GetPOCOsAndOrders();
 
-      for (int i = 1; i <= 20; i++)
-      {
-        var items = new[] { "3", "5" };
-        var combs = Enumerable.Repeat(items, i).CartesianProduct();
-        decimal x = -1;
-        
-        foreach (var item in combs.ToList())
-        {
-          Decimal.TryParse(String.Join("", item), out x);
-          if (IsValid(x)) break;
-          else x = -1;  
-        }
-        
-        Console.WriteLine($"{i} {x}");
-      }
+      var peopleAndOrdersWhereOrderNumberIsThree = items.Where(x => x.Orders.Any(y => orders.Contains(y.Id)));
+
+      //I should only get the last two people out of three and their orders
+      peopleAndOrdersWhereOrderNumberIsThree.ToList().ForEach(x => Console.WriteLine($"{x.Id} {x.Desc} {x.Orders.Count}"));
 
       Console.ReadLine();
     }
     
-    private static void PlayingWithEntity()
-    {
-      using (var context = new TesterEntities())
-      {
-        //Say I just want to project a new object with a select starting from orders and then traversing up.  Not too hard
-        var newObjects = context.teOrders.Where(order => order.OrderId == 1)
-          //SelectMan will FLATTEN a list off of a parent or child in a one to many relationship
-          .SelectMany(peopleInOrderOne => peopleInOrderOne.tePersons)
-          .ToList()
-          .Select(existingPerson => new OtherPerson
-          {
-            PersonId = existingPerson.PersonId,
-            PersonLongName = $"{existingPerson.FirstName} {existingPerson.LastName}",
-            Order = existingPerson.teOrder
-          })
-          .ToList();
-
-        newObjects.ForEach(newPerson => Console.WriteLine($"{newPerson.PersonId} {newPerson.PersonLongName} {newPerson.Order.Description}"));
-
-        // Just an action clause to repeat find items in my context, the important thing to note is that y extends teOrder which is another POCO inside my POCO
-        Action<string, List<tePerson>> GetOrdersForPeople = (header, people) =>
-        {
-          Console.WriteLine(header);
-          people.ForEach(person => Console.WriteLine($"{person.FirstName} {person.LastName} {person.teOrder.Description}"));
-          Console.WriteLine();
-        };
-
-        //I want to look at a person and their orders.  I don't have to do multiple selects down, lazy loading by default gives me a child object off of EF
-        GetOrdersForPeople("First Run", context.tePersons.ToList());
-
-
-        //Say I want a new order for a set of persons in my list?
-        var newOrder = new teOrder { Description = "Shoes" };
-        context.teOrders.Add(newOrder);
-        context.SaveChanges();
-
-        //Now I want to add the new order
-        context.tePersons.SingleOrDefault(person => person.PersonId == 1).teOrder = newOrder;
-        context.SaveChanges();
-
-        //I want to rexamine now
-        GetOrdersForPeople("After changes", context.tePersons.ToList());
-
-        //My newOrder is in memory and I can alter it like clay still and the database will know if I change the context
-        newOrder.Description = "Athletic Shoes";
-        context.SaveChanges();
-
-        GetOrdersForPeople("After changes 2", context.tePersons.ToList());
-
-        //Say I want to update a few people with new orders at the same time
-        var peopleBesidesFirst = context.tePersons.Where(person => person.PersonId != 1).ToList();
-        var firstPersonInList = context.tePersons.Where(person => person.PersonId == 1).ToList();
-
-        var newOrders = new List<teOrder> {
-          new teOrder { Description = "Hat", tePersons = peopleBesidesFirst },
-          new teOrder { Description = "Tie", tePersons = firstPersonInList }
-          };
-
-        context.teOrders.AddRange(newOrders);
-        context.SaveChanges();
-
-        GetOrdersForPeople("After changes 3", context.tePersons.ToList());
-      }
-    }
-
     private static POC SwitchLambdaExample()
     {
       var pocos = GetPOCOs();
