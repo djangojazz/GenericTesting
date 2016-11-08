045d549a04749cac191e5988ba387c8cd1cd4edd
Brett Morin <brettmorin2@gmail.com>
2016-11-08 12:58:31 -0800

    Started PluralSite Class on generics

---
+++ GenericTesting/Generics/CircularBuffer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+
+  public class CircularBuffer
+  {
+    private object[] _buffer;
+    private int _start;
+    private int _end;
+
+    public CircularBuffer() : this(capacity: 10)
+    {
+    }
+
+    public CircularBuffer(int capacity)
+    {
+      _buffer = new object[capacity + 1];
+      _start = 0;
+      _end = 0;
+    }
+
+    public void Write(object value)
+    {
+      _buffer[_end] = value;
+      _end = (_end + 1) % _buffer.Length;
+      if (_end == _start)
+      {
+        _start = (_start + 1) % _buffer.Length;
+      }
+    }
+
+    public object Read()
+    {
+      var result = _buffer[_start];
+      _start = (_start + 1) % _buffer.Length;
+      return result;
+    }
+
+    public int Capacity
+    {
+      get { return _buffer.Length; }
+    }
+
+    public bool IsEmpty
+    {
+      get { return _end == _start; }
+    }
+
+    public bool IsFull
+    {
+      get { return (_end + 1) % _buffer.Length == _start; }
+    }
+  }
+}
---
+++ GenericTesting/Generics/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var buffer = new CircularBuffer(capacity: 3);
+
+      while (true)
+      {
+        var value = 0.0;
+        var input = Console.ReadLine();
+
+        if (double.TryParse(input, out value))
+        {
+          buffer.Write(value);
+          continue;
+        }
+        break;
+      }
+
+      var sum = 0.0;
+      Console.WriteLine("Buffer: ");
+      while (!buffer.IsEmpty)
+      {
+        sum += (Double)buffer.Read();
+      }
+
+      Console.WriteLine(sum);
+    }
+  }
+}
