c31f2356ff64ba565edb82220dca14803e70d9ef
Brett Morin <brettmorin2@gmail.com>
2017-02-08 10:22:58 -0800

    Made changes to controls

---
+++ GenericTesting/WPFControls/Views/Test.xaml.vb
@@ -0,0 +1,21 @@
+﻿Public Class Test
+  Public Sub New()
+
+    ' This call is required by the designer.
+    InitializeComponent()
+
+    ' Add any initialization after the InitializeComponent() call.
+
+  End Sub
+
+  Public Shared ReadOnly TestTitleProperty As DependencyProperty = DependencyProperty.Register("TestTitle", GetType(String), GetType(Test), New UIPropertyMetadata(String.Empty))
+
+  Public Property TestTitle As String
+    Get
+      Return CType(GetValue(TestTitleProperty), String)
+    End Get
+    Set
+      SetValue(TestTitleProperty, Value)
+    End Set
+  End Property
+End Class
