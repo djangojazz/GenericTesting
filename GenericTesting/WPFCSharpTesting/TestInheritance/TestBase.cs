6e2ed6ad28fec511cdd57b32f35422664cf0b460
Brett Morin <brettmorin2@gmail.com>
2017-03-24 07:48:22 -0700

    Tested some inheritance logic

---
+++ GenericTesting/WPFCSharpTesting/TestInheritance/TestBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WPFCSharpTesting.TestInheritance
+{
+  public abstract class TestBase : UserControl
+  {
+    public static readonly DependencyProperty TestProperty = DependencyProperty.Register(nameof(Test), typeof(string), typeof(TestBase), new UIPropertyMetadata(string.Empty));
+    public string Test
+    {
+      get { return Convert.ToString(GetValue(TestProperty)); }
+      set { SetValue(TestProperty, value); }
+    }
+
+    protected abstract void DoIt();
+  }    
+}
