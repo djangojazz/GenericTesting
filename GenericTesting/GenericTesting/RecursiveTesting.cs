a3b0a3335f1b222d4c268431620bc65b0c5c5e76
djangojazz <brettmorin2@gmail.com>
2019-05-30 04:58:32 -0700

    Updated code to find edge case

--- GenericTesting/GenericTesting/RecursiveTesting.cs
+++ GenericTesting/GenericTesting/RecursiveTesting.cs
@@ -31,13 +31,8 @@ namespace GenericTesting
                 var itemsToWorkOn = nodeToWorkOn.SubNodes.Where(x => x.Name != null);
                 if (itemsToWorkOn.Any())
                 {
-                    //var lastClosingElement = sb.ToString().ToCharArray().Reverse().Skip(2).Take(3).ToList();
                     sb.Append($"\"{nodeToWorkOn.Group}\": [{Environment.NewLine}");
                     sb.Append(String.Join($",{Environment.NewLine}", itemsToWorkOn.Select(x => $"{{\"{x.Name}\": \"{x.Value}\"}}")));
-                    
-                    //if (lastClosingElement.Count == 3 && lastClosingElement[0] == '[' && lastClosingElement[2] == ':')
-                    //    sb.Append($"{Environment.NewLine}]");
-                    
                     sb.Append($"{Environment.NewLine}],{Environment.NewLine}");
 
                     node.SubNodes.Remove(nodeToWorkOn);
@@ -141,7 +136,7 @@ namespace GenericTesting
             else
             {
                 var groupOnNode = $"{sub.Substring(0, 1).ToUpper()}{sub.Substring(1, firstSlash - 1).ToLower()}".GetSectionsSeparatedByCaps();
-                var nameAfterGroup = sub.Substring(firstSlash + 1, sub.Length - firstSlash - 1); //.GetSectionsSeparatedByCaps();
+                var nameAfterGroup = sub.Substring(firstSlash + 1, sub.Length - firstSlash - 1);
                 var groupIfExists = node.SubNodes.FirstOrDefault(x => String.Compare(x.Group, groupOnNode, false) == 0);
 
                 if (groupIfExists != null)
