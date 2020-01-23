b0f9a667c39556d428b267bed71befa454dad7e4
djangojazz <brettmorin2@gmail.com>
2020-01-22 20:51:27 -0800

    Checking async activity

---
+++ GenericTesting/Core22Test/GenericExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Core22Test
+{
+    public static class GenericExtensions
+    {
+        public static IList<IList<T>> PartitionList<T>(this IList<T> source, int size = 1000)
+        {
+            var d = new List<IList<T>>();
+
+            for (int i = 0; i < Math.Ceiling(source.Count / (double)size); i++)
+                d.Add(source.Skip(size * i).Take(size).ToList());
+
+            return d;
+        }
+    }
+}
--- GenericTesting/Core22Test/Program.cs
+++ GenericTesting/Core22Test/Program.cs
@@ -1,15 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Core22Test
 {
     class Program
     {
+        private static async Task DoIt(int id)
+        {
+            Console.WriteLine($"Id {id.ToString()}");
+            await Task.Factory.StartNew(() => Thread.Sleep(800));
+        }
+
         static void Main(string[] args)
         {
-            var n = new POCO { Id = 1, Date = "@90DaysPrior", Date2 = "1-2-2019" };
+            var listings = Enumerable.Range(1, 64).ToList();
 
-            var validator = new DynamicOrStaticDate();
-            Console.WriteLine(validator.IsValid(n.Date) && validator.IsValid(n.Date2));
+            //listings.ForEach(DoIt);
+            var partitionSize = listings.Count() / 8;
+            listings.ToList().PartitionList(partitionSize >= 1 ? partitionSize : 1).ToList().ForEach(x =>
+            {
+                Parallel.ForEach(x, async y =>
+                {
+                    await DoIt(y);
+                });
+
+                Console.WriteLine("Done Series");
+            });
+
+            Console.WriteLine("Done");
 
             Console.ReadLine();
         }
