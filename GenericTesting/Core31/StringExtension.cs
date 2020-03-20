18f9cf10d28e0234aec3efcce598b9340ec11aa0
djangojazz <brettmorin2@gmail.com>
2020-03-20 13:19:55 -0700

    updated string extension

---
+++ GenericTesting/Core31/StringExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core31
+{
+    public static class StringExtension
+    {
+        public static string GetSubString(this string input, string start, string end, bool includeBeginning = false, bool includeEnd = false)
+        {
+            var startInd = input.IndexOf(start);
+            var len = input.Length;
+            
+            if (startInd == -1)
+                return null;
+
+            var sub = includeBeginning ? input.Substring(startInd, len - startInd) : input.Substring(startInd + start.Length, len - (startInd + start.Length));
+
+            var endInd = sub.IndexOf(end);
+
+            if (endInd == -1)
+                return null;
+
+            return includeEnd ? sub.Substring(0, (endInd + end.Length)) : sub.Substring(0, endInd);
+        }
+    }
+}
