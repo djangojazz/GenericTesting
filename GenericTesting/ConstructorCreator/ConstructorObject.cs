c81aad7b15c8c896314b71899843d308ee495fe2
BrettAya <brett.morin@ayahealthcare.com>
2020-12-17 17:16:57 -0800

    Updated code for new project

---
+++ GenericTesting/ConstructorCreator/ConstructorObject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorCreator
+{
+    public class ConstructorObject
+    {
+        public string Name { get; set; }
+        public List<Line> Lines { get; set; }
+
+        public ConstructorObject(string name, List<Line> lines) => (Name, Lines) = (name, lines);
+    }
+
+    public class Line
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+
+        public Line(int id, string name, string type) => (Id, Name, Type) = (id, name, type);
+    }
+}
---
+++ GenericTesting/ConstructorCreator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace ConstructorCreator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var location = @"C:\AyaGit\Applications\Aya.Core.Api\Aya.Core.DTO\AyaNova\BizDev\FacilityClientOverviewDto.cs";
+            string s = string.Empty;
+
+            using (var sr = new StreamReader(location))
+            {
+                s = sr.ReadToEnd();
+            }
+
+            var lines = s.Split(Environment.NewLine);
+
+            var hdr = lines.FirstOrDefault(x => x.Contains("public class"));
+            hdr = hdr.Substring(hdr.IndexOf("class"));
+            hdr = hdr.Substring(hdr.IndexOf(' '));
+
+            var props = lines
+                .Where(x => x.Contains("get;"))
+                .Take(5)
+                .Select((x, i) => new { Ind = i+1, Strings = x.Substring(x.IndexOf("public")).Split(" ")})
+                .Select(x => new Line(x.Ind, x.Strings[2], x.Strings[1]))
+                .ToList();
+
+            var c = new ConstructorObject(hdr, props);
+            Console.WriteLine(c.Name);
+            c.Lines.ForEach(x => Console.WriteLine($"{x.Id} {x.Name} {x.Type}"));
+        }
+    }
+}
