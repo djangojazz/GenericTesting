9f1567babb64c5b67534d2906c5b2dbfbdfacaa0
BrettAya <brett.morin@ayahealthcare.com>
2021-01-15 15:35:42 -0800

    Added a reflector converter

--- GenericTesting/ConstructorCreator/Program.cs
+++ GenericTesting/ConstructorCreator/Program.cs
@@ -6,13 +6,41 @@ using System.Text;
 
 namespace ConstructorCreator
 {
+    public class POCO
+    {
+        public int Id { get; set; }
+        public string Desc { get; set; }
+        public DateTime Date { get; set; }
+
+        public POCO() { }
+        public POCO(int id, string desc, DateTime date) => (Id, Desc, Date) = (id, desc, date);
+    }
+
+    public class POCO2
+    {
+        public int Id { get; set; }
+        public string Desc { get; set; }
+        public DateTime Date { get; set; }
+
+        public POCO2() { }
+        public POCO2(int id, string desc) => (Id, Desc) = (id, desc);
+
+        public override string ToString() => $"{Id} {Desc} {Date}";
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
             //Make or check directory
-            var target = DirectoryHelper.MakeOrCreateDirectory(@"C:\Temp");
-            
+            //var target = DirectoryHelper.MakeOrCreateDirectory(@"C:\Temp");
+            var item = new POCO(1, "Abc", DateTime.Now);
+            var item2 = new POCO(2, "Def", DateTime.Now.AddDays(2));
+            var items = new List<POCO> { item, item2 };
+            items.Select(x => ReflectorConverter.Converter<POCO, POCO2>(x)).ToList().ForEach(Console.WriteLine);
+
+            //var output = ReflectorConverter.Converter<POCO, POCO2>(item);
+            //Console.WriteLine(output);
         }
     }
 }
---
+++ GenericTesting/ConstructorCreator/ReflectorConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorCreator
+{
+    public static class ReflectorConverter
+    {
+        public static TOut Converter<TIn, TOut>(TIn input) where TIn : class where TOut : class, new()
+        {
+            var output = new TOut();
+            var propertiesIn = input.GetType().GetProperties().ToList();
+            var propertiesOut = output.GetType().GetProperties().ToList();
+            foreach (var prop in propertiesIn)
+            {
+                var match = propertiesOut.SingleOrDefault(x => x?.Name == prop?.Name && x?.PropertyType == prop?.PropertyType);
+                if (match != null)
+                    match.SetValue(output, GetPropValue(input, prop.Name));
+            }
+
+            return output;
+        }
+
+        private static object GetPropValue(object src, string propName) => src.GetType().GetProperty(propName).GetValue(src, null);
+    }
+}
