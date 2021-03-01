cb1940545f351b4534af080abfaf354de79a08dc
BrettAya <brett.morin@ayahealthcare.com>
2021-03-01 11:36:26 -0800

    Added CSV parser

---
+++ GenericTesting/CSVToJson/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CSVToJson
+{
+    class Program
+    {
+        private static readonly string _alphas = "abcdefghijklmnopqrstuvwxyz";
+        private static List<string> _firstLines = null;
+        
+        static void Main(string[] args)
+        {
+            #region Checking
+            if (args.Length != 2)
+            {
+                Console.WriteLine("You must have two arguments: 1. A file location 2. A bool if the first row has headers");
+                return;
+            }
+
+            var locale = args[0];
+
+            if(!File.Exists(locale))
+            {
+                Console.WriteLine($"The location you gave {locale} does not exist, please give a location that exists");
+                return;
+            }
+
+            if(!bool.TryParse(args[1], out bool headerHasRows))
+            {
+                Console.WriteLine($"The argument you gave of {args[1]} is not a boolean value.  Just use true or false to indicate if the header has descriptors");
+                return;
+            }
+
+            #endregion
+
+            var lines = GetFileDataRows(locale);
+            _firstLines = lines.First().Split(",").Select(x => $"[{x}]").ToList();
+            (var sb, var columnCount) = GetAlphaJsonAndCount(lines, headerHasRows);
+            //(new StringBuilder("Some Data"), 12); //GetAlphaJsonAndCount(lines, true);
+            sb.AppendLine();
+            sb.AppendLine();
+            sb.AppendLine("Select *");
+            sb.AppendLine("From OPENJSON(@Import)");
+            sb.AppendLine("\tWith");
+            sb.AppendLine("\t(");
+            for (int i = 0; i < columnCount; i++)
+            {
+                sb.Append("\t");
+                var lineStart = i == 0 ? "\t" : ",\t";
+                sb.Append($"{lineStart}{GetHeaderIfNeeded(headerHasRows, i)} VARCHAR(256) '$.{_alphas[i]}'{Environment.NewLine}");
+            }
+            sb.AppendLine("\t)");
+
+            Console.WriteLine(sb.ToString());
+        }
+
+
+        private static List<string> GetFileDataRows(string location)
+        {
+            using (var sr = new StreamReader(location))
+            {
+                var s = sr.ReadToEnd();
+                return s.Trim().Split(Environment.NewLine).ToList();
+            }
+        }
+
+        private static (StringBuilder sb, int columnCount) GetAlphaJsonAndCount(List<string> lines, bool firstLineHasData)
+        {
+            var sb = new StringBuilder(string.Empty);
+            var colCount = 0;
+
+            lines.Skip(firstLineHasData ? 1 : 0).ToList().ForEach(x =>
+            {
+                sb.Append("{");
+                var items = x.Split(",");
+                for (int i = 0; i < items.Count(); i++)
+                {
+                    if (i == 0)
+                        colCount = items.Count();
+
+                    sb.Append($"\"{_alphas[i]}\":\"{items[i].Replace("\"", string.Empty)}\",");
+                }
+                sb.Remove(sb.Length - 1, 1);
+                sb.Append($"}},");
+            });
+
+            sb = sb.Remove(sb.Length - 1, 1);
+            sb.Replace("-", ",").Replace("'", "''");
+            sb.Append("]'");
+
+            return (sb = new StringBuilder("Declare @Import Varchar(max) = '[" + sb), colCount);
+        }
+
+        private static string GetHeaderIfNeeded(bool headerHasRows, int i) => headerHasRows ? _firstLines[i] : _alphas[i].ToString();
+    }
+}
--- GenericTesting/ConstructorCreator/Program.cs
+++ GenericTesting/ConstructorCreator/Program.cs
@@ -30,11 +30,44 @@ namespace ConstructorCreator
 
     class Program
     {
+        public static string MakeInstatiationFromObjectWatch(string s)
+        {
+            var items = s.Split(Environment.NewLine).Where(x => x != string.Empty).ToList();
+            var sb = new StringBuilder();
+            items.ForEach(x => sb.AppendLine(x.Substring(0, x.IndexOf('\t')) + " = " + x.Substring(x.IndexOf('\t') + 1) + ", "));
+            return sb.ToString();
+        }
+
+        public static string MakeTestsFromString(string s)
+        {
+            var items = s.Split(Environment.NewLine).Select(x => x.Trim()).Where(x => x.StartsWith("public") && x.Contains(" To")).ToList();
+            var sb = new StringBuilder();
+            items.ForEach(x =>
+            {
+                var indexTo = x.IndexOf(" To");
+                var indexP = x.IndexOf("(");
+                var indexX = x.IndexOf("x)");
+                if (indexTo > 0 && indexP > 0 && indexX > 0 && indexP > indexTo && x.Substring(indexP - 1, 1) != "s")
+                {
+                    var name = x.Substring(indexTo + 3, indexP - indexTo - 3).Trim();
+                    var sigName = x.Substring(indexP + 6, indexX - indexP - 6).Trim();
+                    sb.AppendLine();
+                    sb.AppendLine("[Fact]");
+                    sb.AppendLine("public void ShouldExtract" + name + "() => new " + sigName + "().NotBeNull(x => x.To" + name + "());");
+                }
+
+            });
+
+            return sb.ToString();
+                //[Fact]
+        //public void ShouldExtractJobInfoHistory() => new JobInfo().NotBeNull(x => x.ToJobInfoHistory());
+        }
+
         static void Main(string[] args)
         {
-            //Make or check directory
+            ////Make or check directory
             var target = DirectoryHelper.MakeOrCreateDirectory(@"C:\Temp");
-            DirectoryHelper.CreateConstruction(@"C:\AyaGit\Applications\Aya.Core.Api\Aya.Core.DAL\Entities\Tables\Timecard\PayType.cs", target.FullName);
+            DirectoryHelper.CreateConstruction(@"C:\AyaGit\Applications\Aya.Core.Api\Aya.Core.DTO\Shifts\PerDiem\CandidateDto.cs", target.FullName);
 
             //var output = ReflectorConverter.Converter<POCO, POCO2>(item);
             //Console.WriteLine(output);
