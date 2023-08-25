44fdf33d3fca427ff098dae2b9bc0cf8b1568a61
djangojazz <brettmorin2@gmail.com>
2023-08-25 08:59:44 -0700

    Got working up till explicit call

--- GenericTesting/NET6Console/Program.cs
+++ GenericTesting/NET6Console/Program.cs
@@ -12,19 +12,22 @@ static object MakeComplexMember(Type type)
 
     foreach (var p in props)
     {
-        //Try to get default for value types and primitives
-        var value = p.PropertyType.IsValueType ? Activator.CreateInstance(p.PropertyType) : null;
+        var value = GetValue(p.PropertyType);
 
-        if (value != null)
-            p.SetValue(obj, value);
-
-        if (value == null)
+        if (value != null) //Simple type
         {
-            var propertyName = p.PropertyType.FullName;
-
-            if (propertyName == "System.String")
+            p.SetValue(obj, value);
+        }
+        else //More complex Type
+        {
+            if (p.PropertyType.Name == "Dictionary`2")
             {
-                p.SetValue(obj, "Abc");
+                var t1 = p.PropertyType.GenericTypeArguments[0];
+                var t2 = p.PropertyType.GenericTypeArguments[1];
+                var ty1 = t1.BaseType;
+
+                var d = GetDictionary<string, string>(t1, t2);
+                p.SetValue(obj, d);
             }
             else
             {
@@ -36,11 +39,67 @@ static object MakeComplexMember(Type type)
     return obj ?? string.Empty;
 }
 
+static Dictionary<TKey, TVal> GetDictionary<TKey, TVal>(Type type1, Type type2)
+{
+    var v1 = GetValueGeneric<TKey>(type1);
+    var v2 = GetValueGeneric<TVal>(type2);
+
+    return new Dictionary<TKey, TVal> { { (TKey)(object)v1, (TVal)(object)v2 } };
+}
+
+static object GetValue(Type t)
+{
+    if (t == typeof(string))
+        return " ";
+
+    if (t == typeof(int) || t == typeof(long) || t == typeof(float) || t == typeof(decimal))
+        return 0;
+
+    if (t == typeof(bool))
+        return false;
+
+    if (t == typeof(DateTime) || t == typeof(DateTimeOffset))
+        return DateTime.Now;
+
+    if (t == typeof(object))
+        return new object();
+
+    return null;
+}
+
+static T GetValueGeneric<T>(Type t)
+{
+    //var typeName = (t as Type).Name.ToLower();
+    //var val = (T)(object)null;
+
+    //if (typeName == "string")
+    //    val = (T)(object)" ";
+
+    if (t == typeof(string))
+        return (T)(object)" ";
+
+    //if (t == typeof(int) || t == typeof(long) || t == typeof(float) || t == typeof(decimal) )
+    //    return (T)(object)0;
+
+        //if (t == typeof(bool))
+        //    return (T)(object)false;
+
+        //if (t == typeof(DateTime) || t == typeof(DateTimeOffset)) 
+        //    return (T)(object)DateTime.Now;
+
+        //if (t == typeof(object))
+        //    return (T)(object)new object();
+
+    return (T)(object)null;
+}
+
+
 public class POCO
 {
     public int Id { get; set; }
     public string Desc { get; set; }
-    //public Dictionary<string, string> Data { get; set; }
+    public Dictionary<string, string> Data { get; set; }
+    //public Dictionary<object, DateTime> Data2 { get; set; }
     public MiniPoco Mini { get; set; }
 }
 
