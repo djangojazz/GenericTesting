61f0bca431bacd1f1ff0ff524fa129dae5bdee2f
djangojazz <brettmorin2@gmail.com>
2024-01-28 16:28:45 -0800

    Got basics working

---
+++ GenericTesting/WPFTest/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFTest
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        string _textEntry;
+
+        public MainViewModel() 
+        {
+            TextEntry = "Setup";
+        }
+
+        public string TextEntry
+        {
+            get => _textEntry;
+            set
+            {
+                _textEntry = value;
+                OnPropertyChanged(nameof(TextEntry));
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void OnPropertyChanged(String info)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(info));
+        }
+    }
+}
